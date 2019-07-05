8064987e463efcfb4fe9daed4e5e255f7733a8cc
Blacknon <blacknon@orebibou.com>
2019-07-06 07:08:42 +0900

    v0.1.1

--- hwatch.rb
+++ hwatch.rb
@@ -5,6 +5,9 @@ class Hwatch < Formula
     url "https://github.com/blacknon/hwatch/releases/download/0.1.1/hwatch_0.1.1_darwin_amd64.tar.gz"
     sha256 "1c7a0c600f9c95d5e04936185e71db798d1c324b9053f524bd82051d0cb55fb4"
   end
+
+  depends_on 'ncurses'
+
   def install
     bin.install Dir['hwatch']
   end

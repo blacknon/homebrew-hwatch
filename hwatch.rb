ff0fe63d66936a009537b2b39b84b1b994176371
blacknon <blacknon@orebibou.com>
2022-02-12 14:26:49 +0900

    update v0.3.1

--- hwatch.rb
+++ hwatch.rb
@@ -2,8 +2,8 @@ class Hwatch < Formula
   desc "alternative watch command"
   homepage "https://github.com/blacknon/hwatch"
   if OS.mac?
-      url "https://github.com/blacknon/hwatch/releases/download/0.3.0/hwatch_0.3.0_darwin_amd64.tar.gz"
-    sha256 "ef0a2cd878ba7c4580f4391095be596f08b779f9ed55eb7c3033bf1ae4a7de7e"
+    url "https://github.com/blacknon/hwatch/releases/download/0.3.1/hwatch-0.3.1.x86_64-apple-darwin.tar.gz"
+    sha256 "62b89250d443723b6748a8da842174df0b8f0343e44f00d066faa903eab0104f"
   end
 
   def install
